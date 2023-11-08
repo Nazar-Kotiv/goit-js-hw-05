6692a1111b07c5d15df8f158715b9fe985c86d16
Nazar-Kotiv <kotnazar20@gmail.com>
2023-11-08 15:47:48 +0100

    commit 2

--- js/task-4.js
+++ js/task-4.js
@@ -1,8 +1,8 @@
 
 const getTotalBalanceByGender = (users, gender) => {
-    return users
-    .filter(user => user.gender === gender)
-    .reduce((total, user) => total + user.balance, 0);
+  return users
+  .filter(user => user.gender === gender)
+  .reduce((total, user) => total + user.balance, 0);
 }
 
 const allUsers = [
